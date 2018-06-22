8ee18d4557ff898b4c402369cd51e1e942e98e4b
willesmar <willesmar@gmail.com>
2018-06-22 16:33:18 -0400

    Inicio dev pagina sobre

--- lib/pages/sobre.dart
+++ lib/pages/sobre.dart
@@ -8,24 +8,58 @@ class Sobre extends StatelessWidget {
         title: new Text('Sobre'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: new Container(
-        child: new Center(
-          child: new Icon(
-            Icons.account_balance,
-            size: 150.0,
-            color: Theme.of(context).primaryColor,
+      body: new ListView(children: <Widget>[
+        new Container(
+          constraints: new BoxConstraints.expand(
+            height: 300.0,
           ),
+          child: new Stack(fit: StackFit.expand, children: <Widget>[
+            new Image.asset(
+              'assets/images/iasd_ol3.jpg',
+              fit: BoxFit.cover,
+            ),
+            new Positioned(
+              left: 0.0,
+              right: 0.0,
+              bottom: 0.0,
+              child: new Container(
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.black.withOpacity(0.8),
+                      ]),
+                ),
+                padding: const EdgeInsets.all(18.0),
+                child: new Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    new Expanded(
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          new Text(
+                            'IASD OLIVEIRA III',
+                            style: new TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ]),
         ),
-      ),
+        Text('data'),
+      ]),
     );
-    // return new Container(
-    //   child: new Center(
-    //     child: new Icon(
-    //       Icons.account_balance,
-    //       size: 150.0,
-    //       color: Colors.red,
-    //     ),
-    //   ),
-    // );
   }
 }
